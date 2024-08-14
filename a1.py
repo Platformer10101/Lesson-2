ed0873f24c39b173de4a6c4013a87222c34e65be
Platformer10101 <platformer10101@gmail.com>
2024-08-14 10:58:42 -0400

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,21 @@
+name = "Isaac"
+age = 15
+is_student = True
+weight = 60.3
+#notes
+print("name :",name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("is_student :", is_student)
+print("Data Type of is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+#type casting: turning one type into another
+print("\n After Type Casting...")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type(weight))
