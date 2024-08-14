b671fd7579c3ea38e55330901c737a96525b309d
Platformer10101 <platformer10101@gmail.com>
2024-08-14 14:33:02 -0400

    Add files via upload

---
+++ a3.py
@@ -0,0 +1,15 @@
+first_name = "Isaac"
+last_name = "Chung"
+full_name = first_name+last_name
+example = "He"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name :", full_name)
+print("String multiplied 5 times gives this result :",example)
+
+word = 'coding'
+print("Length of String :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[5])
+print("String Sliced :", word[0:3])
